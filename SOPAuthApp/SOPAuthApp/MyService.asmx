e9f98f6e53ee5361e71b97c336b046cee790f3b3
Intesar Alam <alam.kir@gmail.com>
2019-01-20 18:47:20 +0530

    20012019

---
+++ SOPAuthApp/SOPAuthApp/MyService.asmx
@@ -0,0 +1,2 @@
+﻿
+<%@ WebService Language="C#" CodeBehind="~/App_Code/MyServiceClass.cs" Class="SOPAuthApp.MyServiceClass" %>
---
+++ SOPAuthApp/SOPAuthApp/obj/Release/Package/PackageTmp/App_Code/MyServiceClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+
+namespace SOPAuthApp
+{
+    public class MyServiceClass
+    {
+        public UserDetails User;
+        [WebMethod]
+        [SoapHeader("User", Required = true)]
+        public string SayHello(string userName)
+        {
+            if (User != null)
+            {
+                if (User.IsValid())
+                    return string.Format("Hello...{0} {1} ☺ ", userName,
+                        DateTime.Now.ToString("tt") == "AM" ? " good morning " : " good evening ");
+                else
+                    return "Error in authentication";
+            }
+            else
+            {
+                return "Error in authentication";
+            }
+        }
+    }
+}
